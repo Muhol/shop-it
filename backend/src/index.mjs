0001fe5baba43872014ee2f2534fdd05af53a90b
Muhol <ianmuhol48@gmail.com>
2025-08-15 10:57:22 +0300

    port modefications2

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,31 +1,35 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import connectDB from '../config/db.mjs'
-import router from '../routes/index.mjs'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "../config/db.mjs";
+import router from "../routes/index.mjs";
+import cookieParser from "cookie-parser";
 
-const app = express()
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}))
-app.use(express.json({ limit: '50mb' })); // Set the limit to 50mb or any value you need
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cookieParser())
-app.use("/api", router)
+const app = express();
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL,
+		credentials: true,
+	})
+);
+app.use(express.json({ limit: "50mb" })); // Set the limit to 50mb or any value you need
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(cookieParser());
+app.use("/api", router);
 
 connectDB();
 
-if (process.env.NODE_ENV !== "production"){
-    const PORT = process.env.PORT || 8080
-    connectDB().then(() => { 
-        app.listen(PORT,()=> {
-            console.log(`server is running on port ${PORT}`)
-        })
-    })
+if (process.env.NODE_ENV !== "production") {
+	import("dotenv").then((dotenv) => dotenv.config());
 }
 
- export default app; 
- 
+const PORT = process.env.PORT || 8080;
+
+connectDB().then(() => {
+	app.listen(PORT, "0.0.0.0", () => {
+		console.log(`✅ Server is running on port ${PORT}`);
+	});
+});
+
+export default app;
